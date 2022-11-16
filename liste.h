bae2812e6e9eae8304b4054f86a5baf2f4968619
azizbenazine1 <aziz.benazine@esprit.tn>
2022-11-16 22:58:11 +0100

    le fichier liste

---
+++ liste.h
@@ -0,0 +1,24 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
+
+typedef struct
+{
+char nom_liste[20];
+int id-candidat;
+char nom [20];
+char prenom [20];
+int age [2];
+char orientation [30];
+int id-electeur;//3 id
+int id-list;
+//date
+int nbvot;
+
+}list;
+
+int ajouter(list p , char filename []);
+int modifier(int id, list nouv, char * filename);
+int supprimer(int id, char * filename);
+list  chercher(int id, char * filename);
+
+#endif // POINT_H_INCLUDED
