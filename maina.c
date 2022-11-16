9b5ec5c4dfa5173d9f714e124548f052ef79a44f
azizbenazine1 <aziz.benazine@esprit.tn>
2022-11-16 11:26:21 +0100

    le fichier main

---
+++ maina.c
@@ -0,0 +1,26 @@
+#include "liste.h"
+#include<stdio.h>
+int main()
+{
+
+list  l={2234," benazine","aziz",21,"tajamo3",2554,6678 } , l2= {1920," kais","saied",68,"takatol",5567,7789 } ;
+
+if (ajouter(l,"point.txt")==1)
+printf("ajout valider" );
+
+if (ajouter(l2,"point.txt")==1)
+{
+printf("ajout valider" );
+list l = chercher(2234,"point.txt");
+}
+if (l.id-list == -1)
+printf("inexistant");
+else
+
+printf("%d %s %d %d\n",list.id,list.couleur,list.x,list.y);
+
+if (supprimer(7789,"point.txt")==1 )
+printf("suppression valider");
+
+}
+
