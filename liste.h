282a1a4694c6e88e38a9f110840c4d518ae5ffd5
azizbenazine1 <aziz.benazine@esprit.tn>
2022-11-30 22:55:55 +0100

    fichier liste.h

--- liste.h
+++ liste.h
@@ -3,14 +3,14 @@
 
 typedef struct
 {
-char nom_liste[20];
-int id-candidat;
+char nom_list[20];
+int id_candidat;
 char nom [20];
 char prenom [20];
 int age [2];
 char orientation [30];
-int id-electeur;//3 id
-int id-list;
+int id_electeur;
+int id_list;
 //date
 int nbvot;
 
